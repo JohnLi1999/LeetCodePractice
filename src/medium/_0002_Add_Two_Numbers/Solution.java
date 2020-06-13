8390495226ef382ee50e1b7d662ebd386d056837
zhiwenli <lizhiwenzg@gmail.com>
2020-06-13 12:17:24 -0700

    Update 0002 description

--- src/medium/_0002_Add_Two_Numbers/Solution.java
+++ src/medium/_0002_Add_Two_Numbers/Solution.java
@@ -2,10 +2,10 @@ package medium._0002_Add_Two_Numbers;
 
 public class Solution {
     /*  https://leetcode.com/problems/add-two-numbers/
-        Time complexity: O(max(m, n))
+        Time Complexity: O(max(m, n))
             Assume that m and n represents the length of l1 and l2 respectively,
             the algorithm above iterates at most max(m, n) times.
-        Space complexity: O(max(m,n))
+        Space Complexity: O(max(m,n))
             The length of the new list is at most max(m,n)+1.
      */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
