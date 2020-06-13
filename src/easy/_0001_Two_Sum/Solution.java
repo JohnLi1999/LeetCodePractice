e6ac45af2255337dd34f2502cb9f2f30249a79aa
zhiwenli <lizhiwenzg@gmail.com>
2020-06-13 12:16:57 -0700

    Update 0001 Description

--- src/easy/_0001_Two_Sum/Solution.java
+++ src/easy/_0001_Two_Sum/Solution.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public class Solution {
     /*  https://leetcode.com/problems/two-sum/
-        Time complexity : O(n)
+        Time Complexity : O(n)
             We traverse the list containing n elements only once.
             Each look up in the table costs only O(1) time.
-        Space complexity : O(n).
+        Space Complexity : O(n).
             The extra space required depends on the number of items stored in the hash table,
             which stores at most n elements.
      */
