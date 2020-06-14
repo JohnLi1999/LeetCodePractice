7702d3f2af6b14b3947d80caec689a3983f86813
zhiwenli <lizhiwenzg@gmail.com>
2020-06-13 23:17:08 -0700

    Update 0001

--- src/easy/_0001_Two_Sum/Solution.java
+++ src/easy/_0001_Two_Sum/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
         Time Complexity : O(n)
             We traverse the list containing n elements only once.
             Each look up in the table costs only O(1) time.
-        Space Complexity : O(n).
+        Space Complexity : O(n)
             The extra space required depends on the number of items stored in the hash table,
             which stores at most n elements.
      */
