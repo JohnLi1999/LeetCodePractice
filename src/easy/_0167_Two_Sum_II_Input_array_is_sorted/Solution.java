4c7302691d5728a271a728ed2b2751327d93bf6f
zhiwenli <lizhiwenzg@gmail.com>
2020-06-14 12:45:55 -0700

    Update 0167

--- src/easy/_0167_Two_Sum_II_Input_array_is_sorted/Solution.java
+++ src/easy/_0167_Two_Sum_II_Input_array_is_sorted/Solution.java
@@ -2,20 +2,24 @@ package easy._0167_Two_Sum_II_Input_array_is_sorted;
 
 public class Solution {
     /*  https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-        Time Complexity: O(n)
-        Space Complexity: O(1)
+        Time complexity: O(n)
+            Each of the nn elements is visited at most once, thus the time complexity is O(n).
+        Space complexity: O(1)
+            We only use two indexes, the space complexity is O(1).
      */
     public static int[] twoSum(int[] numbers, int target) {
-        // Use two-point strategy
-        int i = 0, j = numbers.length - 1;
+        // Use Two Pointers strategy
+        int low = 0, high = numbers.length - 1;
 
-        while (i < j) {
-            if (numbers[i] + numbers[j] > target) {
-                j--;
-            } else if (numbers[i] + numbers[j] < target) {
-                i++;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+
+            if (sum > target) {
+                high--;
+            } else if (sum < target) {
+                low++;
             } else {
-                return new int[] {i + 1, j + 1};
+                return new int[] {low + 1, high + 1};
             }
         }
 
