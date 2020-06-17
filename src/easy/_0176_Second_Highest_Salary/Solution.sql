ee64e3a16715d356695bfb1b7868778d050ecfa3
zhiwenli <lizhiwenzg@gmail.com>
2020-06-16 17:36:48 -0700

    0176 Second Highest Salary

---
+++ src/easy/_0176_Second_Highest_Salary/Solution.sql
@@ -0,0 +1,14 @@
+# Way 1
+SELECT MAX(Salary) as SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary)
+                FROM Employee)
+
+# Way 2
+SELECT
+    IFNULL(
+        (SELECT DISTINCT Salary
+         FROM Employee
+         ORDER BY Salary DESC
+         LIMIT 1, 1),
+    NULL) AS SecondHighestSalary
