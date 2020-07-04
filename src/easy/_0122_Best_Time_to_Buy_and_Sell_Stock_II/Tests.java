17410eda6703f6ff142ec7eb40d87abbc5a5bf04
zhiwenli <lizhiwenzg@gmail.com>
2020-07-03 22:49:50 -0700

    0122 Best Time to Buy and Sell Stock II

---
+++ src/easy/_0122_Best_Time_to_Buy_and_Sell_Stock_II/Tests.java
@@ -0,0 +1,31 @@
+package easy._0122_Best_Time_to_Buy_and_Sell_Stock_II;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static easy._0122_Best_Time_to_Buy_and_Sell_Stock_II.Solution.maxProfit_Peek_Valley_Approach;
+import static easy._0122_Best_Time_to_Buy_and_Sell_Stock_II.Solution.maxProfit_Simple_One_Pass;
+
+class Tests {
+
+    @Test
+    void test1() {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        assertEquals(7, maxProfit_Peek_Valley_Approach(prices));
+        assertEquals(7, maxProfit_Simple_One_Pass(prices));
+    }
+
+    @Test
+    void test2() {
+        int[] prices = {1, 2, 3, 4, 5};
+        assertEquals(4, maxProfit_Peek_Valley_Approach(prices));
+        assertEquals(4, maxProfit_Simple_One_Pass(prices));
+    }
+
+    @Test
+    void test3() {
+        int[] prices = {7, 6, 4, 3, 1};
+        assertEquals(0, maxProfit_Peek_Valley_Approach(prices));
+        assertEquals(0, maxProfit_Simple_One_Pass(prices));
+    }
+}
