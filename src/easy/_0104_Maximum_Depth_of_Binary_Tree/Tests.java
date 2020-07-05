cb71718556cee4c087a0e59adaad14b994a025fb
zhiwenli <lizhiwenzg@gmail.com>
2020-07-05 09:42:59 -0700

    Update 0104 - Added Iterative Approach

--- src/easy/_0104_Maximum_Depth_of_Binary_Tree/Tests.java
+++ src/easy/_0104_Maximum_Depth_of_Binary_Tree/Tests.java
@@ -3,7 +3,8 @@ package easy._0104_Maximum_Depth_of_Binary_Tree;
 import org.junit.jupiter.api.Test;
 import utils.tree_node.TreeNode;
 
-import static easy._0104_Maximum_Depth_of_Binary_Tree.Solution.maxDepth;
+import static easy._0104_Maximum_Depth_of_Binary_Tree.Solution.maxDepth_Recursive;
+import static easy._0104_Maximum_Depth_of_Binary_Tree.Solution.maxDepth_Iterative;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Tests {
@@ -11,6 +12,12 @@ class Tests {
     @Test
     void test1() {
         TreeNode root = TreeNode.arrayToTree(new Integer[] {3, 9, 20, null, null, 15, 7});
-        assertEquals(3, maxDepth(root));
+        assertEquals(3, maxDepth_Recursive(root));
+    }
+
+    @Test
+    void test2() {
+        TreeNode root = TreeNode.arrayToTree(new Integer[] {3, 9, 20, null, null, 15, 7});
+        assertEquals(3, maxDepth_Iterative(root));
     }
 }
