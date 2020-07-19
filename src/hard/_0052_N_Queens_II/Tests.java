032b6582a0c43a6aa7f3cdf277e33f6de5b6cffd
zhiwenli <lizhiwenzg@gmail.com>
2020-07-18 17:28:07 -0700

    Update 0052 - Added bitwise solution

--- src/hard/_0052_N_Queens_II/Tests.java
+++ src/hard/_0052_N_Queens_II/Tests.java
@@ -7,21 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Tests {
 
+    Solution_Bitwise solution_bitwise = new Solution_Bitwise();
+
     @Test
     void test1() {
         int n = 2;
         assertEquals(0, totalNQueens(n));
+        assertEquals(0, solution_bitwise.totalNQueens(n));
     }
 
     @Test
     void test2() {
         int n = 4;
         assertEquals(2, totalNQueens(n));
+        assertEquals(2, solution_bitwise.totalNQueens(n));
     }
 
     @Test
     void test3() {
         int n = 8;
         assertEquals(92, totalNQueens(n));
+        assertEquals(92, solution_bitwise.totalNQueens(n));
     }
 }
