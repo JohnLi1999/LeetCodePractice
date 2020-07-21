53367016478e1b93e7f2e452b599b08d4ba1d2d8
zhiwenli <lizhiwenzg@gmail.com>
2020-07-20 23:19:13 -0700

    Update 0121 - added dynamic programming solution

--- src/easy/_0121_Best_Time_to_Buy_and_Sell_Stock/Tests.java
+++ src/easy/_0121_Best_Time_to_Buy_and_Sell_Stock/Tests.java
@@ -4,10 +4,38 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static easy._0121_Best_Time_to_Buy_and_Sell_Stock.Solution_Dynamic_Programming.maxProfit;
 import static easy._0121_Best_Time_to_Buy_and_Sell_Stock.Solution.maxProfit_BruteForce;
 import static easy._0121_Best_Time_to_Buy_and_Sell_Stock.Solution.maxProfit_OnePass;
 
 class Tests {
+
+    @Nested
+    class Dynamic_Programming {
+        @Test
+        void test1() {
+            int[] prices = {7, 1, 5, 3, 6, 4};
+            assertEquals(5, maxProfit(prices));
+        }
+
+        @Test
+        void test2() {
+            int[] prices = {7, 6, 4, 3, 1};
+            assertEquals(0, maxProfit(prices));
+        }
+
+        @Test
+        void test3() {
+            int[] prices = {2, 8, 1, 4, 6, 3};
+            assertEquals(6, maxProfit(prices));
+        }
+
+        @Test
+        void test4() {
+            int[] prices = {2, 8, 1, 4, 6, 3, 9};
+            assertEquals(8, maxProfit(prices));
+        }
+    }
 
     @Nested
     class One_Pass {
