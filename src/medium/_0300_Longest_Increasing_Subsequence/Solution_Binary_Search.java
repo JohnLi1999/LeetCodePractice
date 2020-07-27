4d2ac8f4c7398ce1c3afea1ccff06bf2a0b6e280
zhiwenli <lizhiwenzg@gmail.com>
2020-07-27 15:59:25 -0700

    0300 Longest Increasing Subsequence

---
+++ src/medium/_0300_Longest_Increasing_Subsequence/Solution_Binary_Search.java
@@ -0,0 +1,58 @@
+package medium._0300_Longest_Increasing_Subsequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_Binary_Search {
+    /*  Time complexity: O(n*log(n))
+            Binary search takes log(n) time and it is called n times.
+        Space complexity: O(n)
+            The size of the sequence can be up to n.
+     */
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> LIS = new ArrayList<>();
+
+        for (int num : nums) {
+            int index = binarySearch(LIS, num);
+
+            if (index == LIS.size()) {
+                LIS.add(num);
+            } else {
+                LIS.set(index, num);
+            }
+        }
+
+        return LIS.size();
+    }
+
+    private static int binarySearch(List<Integer> list, int num) {
+        if (list.size() == 0) {
+            return 0;
+        }
+
+        if (num < list.get(0)) {
+            return 0;
+        }
+
+        if (num > list.get(list.size() - 1)) {
+            return list.size();
+        }
+
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (num == list.get(middle)) {
+                return middle;
+            } else if (num > list.get(middle)) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return right + 1;
+    }
+}
