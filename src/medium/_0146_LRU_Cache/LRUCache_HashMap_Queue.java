95b7122a6baf6a16f236c1bcc17bb949b9a6eb9f
zhiwenli <lizhiwenzg@gmail.com>
2020-08-07 16:55:44 -0700

    0146 LRU Cache

--- src/medium/_0146_LRU_Cache/LRUCache_HashMap_Queue.java
+++ src/medium/_0146_LRU_Cache/LRUCache_HashMap_Queue.java
@@ -4,18 +4,27 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class LRUCache {
+public class LRUCache_HashMap_Queue {
+    /*  https://leetcode.com/problems/lru-cache/
+        Time complexity: O(capacity)
+            Both for get and put operations
+            In both operations, we need to search for the position of key and update it in the queue.
+            This might takes O(capacity) time complexity
+        Space complexity: O(capacity)
+            Since the space is used for a hashmap and a queue with capacity elements
+     */
 
     private final HashMap<Integer, Integer> map;
     private final Deque<Integer> keys;
     private final int capacity;
 
-    public LRUCache(int capacity) {
+    public LRUCache_HashMap_Queue(int capacity) {
         this.capacity = capacity;
         map = new HashMap<>();
         keys = new LinkedList<>();
     }
 
+    /* Time Complexity: O(capacity) */
     public int get(int key) {
         if (map.containsKey(key)) {
             keys.remove(key);
@@ -27,6 +36,7 @@ public class LRUCache {
         return -1;
     }
 
+    /* Time Complexity: O(capacity) */
     public void put(int key, int value) {
         if (map.containsKey(key)) {
             keys.remove(key);
