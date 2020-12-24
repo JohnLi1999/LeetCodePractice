6192fe075eca5aa05bafdf34bb2585d8d0d59fdc
zhiwenli <lizhiwenzg@gmail.com>
2020-12-24 12:07:05 -0800

    Update 0198

--- src/medium/_0198_House_Robber/Solution_DP.java
+++ src/medium/_0198_House_Robber/Solution_DP.java
@@ -41,10 +41,6 @@ public class Solution_DP {
             return nums[0];
         }
 
-        if (nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
